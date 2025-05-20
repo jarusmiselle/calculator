339e4b011d226becdbd70fe6adc1bb67853a57be
Ramin Hafezi <wisepersian1369@gmail.com>
2025-05-20 02:51:35 +0000

    TestFactorial

---
+++ pkg/calculator/operations_test.go
@@ -0,0 +1,51 @@
+package calculator
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestFactorial(t *testing.T) {
+	tt := []struct {
+		input       int
+		expected    int
+		expectedErr error
+	}{{
+		input:       -2,
+		expected:    0,
+		expectedErr: errors.New("-2 should be zero or higher"),
+	}, {
+		input:    0,
+		expected: 1,
+	}, {
+		input:    1,
+		expected: 1,
+	}, {
+		input:    3,
+		expected: 6,
+	}, {
+		input:    4,
+		expected: 24,
+	}, {
+		input:    5,
+		expected: 120,
+	}}
+
+	for _, c := range tt {
+		res, err := Factorial(c.input)
+
+		if res != c.expected {
+			t.Fatal("response did not match expected")
+		}
+
+		if c.expectedErr != nil {
+			if err == nil || c.expectedErr.Error() != err.Error() {
+				t.Fatal("error did not match expectedErr")
+			}
+		} else {
+			if err != nil {
+				t.Fatal("error was not nil")
+			}
+		}
+	}
+}
