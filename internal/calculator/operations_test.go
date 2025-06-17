e89ab517f1be54f5484cae6eafd0a2e1ff0359ad
jarusmiselle <geekpharaoh@gmail.com>
2025-06-17 03:24:42 +0000

    added factorial test case and added test for add

--- internal/calculator/operations_test.go
+++ internal/calculator/operations_test.go
@@ -7,7 +7,64 @@ import (
 	"testing"
 )
 
+func TestAdd(t *testing.T) {
+	t.Parallel()
+
+	tt := []struct {
+		input       []float64
+		expected    float64
+		expectedErr error
+	}{{
+		input:    []float64{5, 36},
+		expected: 41,
+	}, {
+		input:    []float64{27, 42},
+		expected: 69,
+	}, {
+		input:    []float64{47, -12},
+		expected: 35,
+	}, {
+		input:       []float64{7, 5, 12},
+		expected:    0,
+		expectedErr: errors.New("usage: calc add left right"),
+	}, {
+		input:       []float64{42},
+		expected:    0,
+		expectedErr: errors.New("usage: calc add left right"),
+	}, {
+		input:       []float64{46, 29, 5, 7, 10},
+		expected:    0,
+		expectedErr: errors.New("usage: calc add left right"),
+	}}
+
+	for _, c := range tt {
+		name := fmt.Sprintf("input: %f, expected: %f, err: %v", c.input, c.expected, c.expectedErr)
+		log.Println(name)
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			res, err := add(c.input)
+
+			if res != c.expected {
+				t.Fatal("response did not match expected")
+			}
+
+			if c.expectedErr != nil {
+				if err == nil || c.expectedErr.Error() != err.Error() {
+					t.Fatalf("error %s did not match expectedErr %s", c.expectedErr, err)
+				}
+			} else {
+				if err != nil {
+					t.Fatal("error was not nil")
+				}
+			}
+		})
+	}
+}
+
 func TestFactorial(t *testing.T) {
+	t.Parallel()
+
 	tt := []struct {
 		input       []float64
 		expected    float64
@@ -31,12 +88,18 @@ func TestFactorial(t *testing.T) {
 	}, {
 		input:    []float64{5},
 		expected: 120,
+	}, {
+		input:       []float64{4, 3},
+		expected:    0,
+		expectedErr: errors.New("usage: calc factorial arg"),
 	}}
 
 	for _, c := range tt {
 		name := fmt.Sprintf("input: %f, expected: %f, err: %v", c.input, c.expected, c.expectedErr)
 		log.Println(name)
 		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
 			res, err := factorial(c.input)
 
 			if res != c.expected {
