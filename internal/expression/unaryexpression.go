8e222b6d3ceeead4eb19df6305010b4489c55366
jarusmiselle <geekpharaoh@gmail.com>
2025-08-26 04:20:25 +0000

    return 0, looped factorial, inlined stringify

--- internal/expression/unaryexpression.go
+++ internal/expression/unaryexpression.go
@@ -26,7 +26,7 @@ func NewUnaryExpression(operation string, operand Expression) (UnaryExpression, 
 func (ue UnaryExpression) Evaluate() (float64, error) {
 	v, err := ue.operand.Evaluate()
 	if err != nil {
-		return v, err
+		return 0, err
 	}
 
 	return ue.operation.evaluate(v)
@@ -43,48 +43,39 @@ type unaryOperation struct {
 }
 
 func factorial(operand float64) (float64, error) {
-	var res float64
-
 	if operand != math.Trunc(operand) {
-		return res, fmt.Errorf("%f is invalid. factorial is only defined for integers", operand)
+		return 0, fmt.Errorf("%f is invalid. factorial is only defined for integers", operand)
 	}
 
 	if operand < 0 {
-		return res, fmt.Errorf("%f should be zero or higher", operand)
+		return 0, fmt.Errorf("%f should be zero or higher", operand)
 	}
 
 	return float64(factorialInt(uint64(operand))), nil
 }
 
 func factorialInt(operand uint64) uint64 {
-	if operand <= 1 {
-		return 1
+	var res uint64 = 1
+	var i uint64 = 2
+	for ; i <= operand; i++ {
+		res *= i
 	}
-
-	return operand * factorialInt(operand)
-}
-
-func factorialString(op Expression) string {
-	return fmt.Sprintf("%s!", op)
+	return res
 }
 
 func negate(operand float64) (float64, error) {
 	return -operand, nil
 }
 
-func negateString(op Expression) string {
-	return fmt.Sprintf("-%s", op)
-}
-
 var unaryOperations = map[string]unaryOperation{
 	"!": {
 		rank:      1,
 		evaluate:  factorial,
-		stringify: factorialString,
+		stringify: func(e Expression) string { return fmt.Sprintf("%s!", e) },
 	},
 	"-": {
 		rank:      2,
 		evaluate:  negate,
-		stringify: negateString,
+		stringify: func(e Expression) string { return fmt.Sprintf("-%s", e) },
 	},
 }
